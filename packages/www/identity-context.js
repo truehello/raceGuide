efff5b8fd5aad6aff206bf26771b0f6e6ecdf64c
truehello <trudelmarc@gmail.com>
2020-02-20 15:16:59 -0800

    addressed netlify identidy bug

--- packages/www/identity-context.js
+++ packages/www/identity-context.js
@@ -12,9 +12,14 @@ const IdentityProvider = props => {
     netlifyIdentity.init({});
   });
   netlifyIdentity.on("login", user => {
+    netlifyIdentity.close();
     setUser(user);
   });
-  netlifyIdentity.on("logout", () => setUser());
+  netlifyIdentity.on("logout", () => {
+    netlifyIdentity.close();
+    setUser();
+  });
+
 
   return (
     <IdentityContext.Provider value={{ identity: netlifyIdentity, user }}>
