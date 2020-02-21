30c96b8c8a8fd76cea0001243a6cae3fffd6f818
truehello <trudelmarc@gmail.com>
2020-02-21 13:11:00 -0800

    added Races to graphql api

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -1,30 +1,66 @@
-const { ApolloServer, gql } = require('apollo-server-lambda');
- 
+const { ApolloServer, gql } = require("apollo-server-lambda");
+
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
-    hello: String
+    Races: [Race]!
+  }
+  type Race {
+    id: ID!
+    name: String!
+    location: String!
+    city: String
+    country: String
+    latitude: Int
+    longitude: Int
+    description: String
+    image: String
+    date: String
+    url: String
+    createdAt: DateTime!
+    updatedAt: DateTime!
+  }
+  type Mutation {
+    createRace(name: String!): Race
+    updateRace(id: ID!): Race
+    deleteRace(id: ID!): Race
   }
 `;
- 
+
+const races = {};
+let racesIndex = 0;
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    Races: () => {
+      return Object.values(Races);
+    }
   },
+  Mutation: {
+    addTodo: (_, { text }) => {
+      racesIndex++;
+      const id = `key-${racesIndex}`;
+      races[id] = { id, text, done: false };
+      return todos[id];
+    },
+    updateTodoDone: (_, { id }) => {
+        races[id].done = true;
+      return races[id];
+    }
+  }
 };
- 
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  
+
   // By default, the GraphQL Playground interface and GraphQL introspection
   // is disabled in "production" (i.e. when `process.env.NODE_ENV` is `production`).
   //
   // If you'd like to have GraphQL Playground and introspection enabled in production,
   // the `playground` and `introspection` options must be set explicitly to `true`.
   playground: true,
-  introspection: true,
+  introspection: true
 });
- 
+
 exports.handler = server.createHandler();
