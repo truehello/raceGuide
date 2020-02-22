68f92aee8f29465dd68563882da9879567e29ca6
truehello <trudelmarc@gmail.com>
2020-02-21 17:37:07 -0800

    implemented user authentication on API

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -1,7 +1,25 @@
 const { ApolloServer, gql } = require("apollo-server-lambda");
 
+//example of a query reliant on logged in user
+// type Query {
+// Races: async (parent, args, { user }) => {
+//     if (!user) {
+//       return [];
+//     } else {
+//       const results = await client.query(
+//         q.Paginate(q.Match(q.Index("Races_by_user"), user))
+//       );
+//       return results.data.map(([ref, text, done]) => ({
+//         id: ref.id,
+//         text,
+//         done
+//       }));
+//     }
+//   }
+// }
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
+
   type Query {
     Races: [Race]!
   }
@@ -44,23 +62,38 @@ const resolvers = {
       return todos[id];
     },
     updateRace: (_, { id }) => {
-        Races[id].done = true;
+      Races[id].done = true;
       return races[id];
     }
   }
 };
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+    typeDefs,
+    resolvers,
+    //this is the Lambds context
+    // we have a client context and a user context. if there is a user avaialble
+    context: ({ context }) => {
+      if (context.clientContext.user) {
+          //if there is user available we return user id
+        return { user: context.clientContext.user.sub };
+      } else {
+          //if not we return empty object
+        return {};
+      }
+    },
+    // By default, the GraphQL Playground interface and GraphQL introspection
+    // is disabled in "production" (i.e. when `process.env.NODE_ENV` is `production`).
+    //
+    // If you'd like to have GraphQL Playground and introspection enabled in production,
+    // the `playground` and `introspection` options must be set explicitly to `true`.
+    playground: true,
+    introspection: true
+  });
 
-  // By default, the GraphQL Playground interface and GraphQL introspection
-  // is disabled in "production" (i.e. when `process.env.NODE_ENV` is `production`).
-  //
-  // If you'd like to have GraphQL Playground and introspection enabled in production,
-  // the `playground` and `introspection` options must be set explicitly to `true`.
-  playground: true,
-  introspection: true
+exports.handler = server.createHandler({
+  cors: {
+    origin: "*",
+    credentials: true
+  }
 });
-
-exports.handler = server.createHandler();
--- packages/www/gatsby-browser.js
+++ packages/www/gatsby-browser.js
@@ -1,4 +1,47 @@
 const React = require("react");
+const {
+  ApolloProvider,
+  ApolloClient,
+  HttpLink,
+  InMemoryCache
+} = require("@apollo/client");
+
+//use this context to set authentication link that we pass to apollo client
+const { setContext } = require("apollo-link-context");
+const netlifyIdentity = require("netlify-identity-widget");
+
 const wrapRootElement = require("./wrap-root-element");
 
-exports.wrapRootElement = wrapRootElement;
+//use setcontext to get the headers
+const authLink = setContext((_, { headers }) => {
+//becasue this link is fired on every request we can use Netlif identity user object
+  const user = netlifyIdentity.currentUser();
+  const token = user.token.access_token;
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+        //spread any headers that already exist into headers object
+      ...headers,
+      // set up our on authorization header. if we have a token we set the bearer token of it is blank
+      Authorization: token ? `Bearer ${token}` : ""
+    }
+  };
+});
+
+const httpLink = new HttpLink({
+  uri: "https://raceguide.netlify.com/.netlify/functions/graphql"
+});
+
+//send the client the concatenated authlink with the http link to the api
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink)
+});
+
+exports.wrapRootElement = ({ element }) => {
+  return (
+    <ApolloProvider client={client}>
+      {wrapRootElement({ element })}
+    </ApolloProvider>
+  );
+};
